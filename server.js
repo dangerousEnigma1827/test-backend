85dfcb1ec8fe1432a91e24c4ab1ac0a726d31e37
Muneeb <syedmuneebmalik1827@gmail.com>
2026-06-15 15:34:47 +0530

    another one

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import connectDB from "./db/connect.js";
+import User from "./models/User.js";
 
 dotenv.config();
 
@@ -8,17 +10,55 @@ const app = express();
 
 app.use(express.json());
 
+// app.use(cors({
+//   origin: process.env.CLIENT_URL,
+//   credentials: true
+// }));
+
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://test-frontend-tau-pearl.vercel.app"
+];
+
 app.use(cors({
-  origin: process.env.CLIENT_URL,
+  origin: allowedOrigins,
   credentials: true
 }));
 
-app.get("/", (req,res)=>{
-  res.json({message:"I Love You Vanshika"});
+// connect DB BEFORE routes
+connectDB();
+
+app.get("/", async (req, res) => {
+  try {
+    const newUser = await User.create({
+      name: "Muneeb " + Date.now()
+    });
+
+    res.json({
+      message: "User created",
+      data: newUser
+    });
+
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+app.get("/test-db", async (req, res) => {
+  res.json({ message: "DB route working" });
+});
+
+app.get("/users", async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 });
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, ()=>{
+app.listen(PORT, () => {
   console.log(`Running on ${PORT}`);
 });
